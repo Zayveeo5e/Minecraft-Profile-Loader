2dea1b18880d6d4a7b620515263607ba56c5f1c5
Zayveeo5e <37855671+Zayveeo5e@users.noreply.github.com>
2019-10-13 22:04:07 -0500

    Clean up Profile creator and make more user friendly

--- Create Profile.bat
+++ Create Profile.bat
@@ -1,8 +1,16 @@
-
+@echo off
 cd "%~dp0\CoreUtils"
-
+cls
+echo Copying files...
 .\cp.exe -r "%~1" "%~dp0\CoreUtils\modpacks"
-
+cls
+echo Creating Loader file...
 .\cp.exe "%~dp0\CoreUtils\base loader.bat" "%~dp0\CoreUtils\ModpackSwitchers\%~nx1.bat"
-
+cls
+echo creating Deleter file...
 .\cp.exe "%~dp0\CoreUtils\base deleter.bat" "%~dp0\CoreUtils\ModpackDeleters\%~nx1.bat"
+cls
+echo Done!
+echo Profile "%~nx1" has been created.
+pause
+call "%~dp0\Launcher.bat"
